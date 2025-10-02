be6a44c75fced266083c2f1604bc10fc0971db9c
laicethecrue <laice.ndombi@gmail.com>
2025-10-02 16:01:07 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,12 +8,12 @@ self.addEventListener("install", e=>{
 
 self.addEventListener("activate", e=>{
   e.waitUntil(
-    caches.keys().then(keys=>
+    caches.keys().then(keys =>
       Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
     )
   );
 });
 
 self.addEventListener("fetch", e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
