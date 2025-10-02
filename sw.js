5bccc8bace4c8d319aa8a5ad01b141c420af3c92
laicethecrue <laice.ndombi@gmail.com>
2025-10-01 20:11:25 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,16 @@
-const CACHE = "oksens-v4"; 
-const ASSETS = [
-  './', './index.html', './styles.css', './app.js',
-  './data.json', './manifest.webmanifest'
+const CACHE = "oksens-v8";
+const FILES = [
+  "/oksens/",
+  "/oksens/index.html",
+  "/oksens/styles.css",
+  "/oksens/app.js",
+  "/oksens/data.json"
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
-      const copy = resp.clone();
-      caches.open(CACHE).then(c => c.put(e.request, copy));
-      return resp;
-    }))
-  );
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
